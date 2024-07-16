77ffa7af0b57e4d639c1428e78c72017fd667e68
Rezo Joglidze <rezojoglidze7@gmail.com>
2024-07-17 01:41:10 +0400

    make minor change

--- Package.swift
+++ Package.swift
@@ -10,14 +10,12 @@ let package = Package(
         .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)
     ],
     
-    /*
     products: [
          // Products define the executables and libraries a package produces, and make them visible to other packages.
          .library(
              name: "RJSwiftMacros",
              targets: ["RJSwiftMacros"])
      ],
-     */
     
     dependencies: [
         // Dependencies declare other packages that this package depends on.
