f7203411b11843cf3be3a8db815327c8e9450a9e
Rezo Joglidze <rezojoglidze7@gmail.com>
2024-07-26 01:24:44 +0400

    Add macros descriptions in the comment

--- Sources/RJSwiftMacros/CodingKeysGeneration/CodingKeysGeneration.swift
+++ Sources/RJSwiftMacros/CodingKeysGeneration/CodingKeysGeneration.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
+/// - Generate `CodingKeys`  with ``CodingKeys()`` Macro
+/// - Adjust coding key with ``CodingKeyProperty(_:)``
+/// - To ignore coding key generation at the specific property use ``CodingKeyIgnored()``
+///
+/// ```
+/// @CodingKeys
+/// struct Car {
+///     let name: String
+///     @CodingKeyProperty("second_name") let surname: String
+///     @CodingKeyIgnored() let color: String
+/// }
+/// ```
 @attached(member, names: named(CodingKeys))
 public macro CodingKeys() = #externalMacro(module: "RJSwiftMacrosImpl", type: "CodingKeysMacro")
 
--- Sources/RJSwiftMacrosImpl/Plugin.swift
+++ Sources/RJSwiftMacrosImpl/Plugin.swift
@@ -5,15 +5,19 @@
 //  Created by Rezo Joglidze on 16.07.24.
 //
 
-import SwiftCompilerPlugin
-import SwiftSyntaxMacros
-
-// MARK: - Plugin
-@main
-struct Plugin: CompilerPlugin {
-    let providingMacros: [Macro.Type] = [
-        CodingKeysMacro.self,
-        CodingKeyPropertyMacro.self,
-        CodingKeyIgnoredMacro.self
-    ]
-}
+#if canImport(SwiftCompilerPlugin)
+    import SwiftCompilerPlugin
+    import SwiftSyntaxMacros
+    
+    /// **Plugin**
+    @main
+    struct Plugin: CompilerPlugin {
+        let providingMacros: [Macro.Type] = [
+            
+            // CodingKeys Macros
+            CodingKeysMacro.self,
+            CodingKeyPropertyMacro.self,
+            CodingKeyIgnoredMacro.self
+        ]
+    }
+#endif
