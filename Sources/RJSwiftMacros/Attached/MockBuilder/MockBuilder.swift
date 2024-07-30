2c55975b8e8b44de957afc0e988b46b4a9831f41
Rezo Joglidze <rezojoglidze7@gmail.com>
2024-07-30 11:45:40 +0400

    Add  definition to the macros

--- Sources/RJSwiftMacros/Attached/MockBuilder/MockBuilder.swift
+++ Sources/RJSwiftMacros/Attached/MockBuilder/MockBuilder.swift
@@ -8,6 +8,26 @@
 import Foundation
 import RJSwiftCommon
 
+/// `MockBuilder(numberOfItems: Int, dataGeneratorType: .random)`: Generates an array of mock data from our models.
+///
+/// ```
+/// @MockBuilder(numberOfItems: 3, dataGeneratorType: .random)
+/// struct Person {
+///     let name: String
+///     let surname: String
+///
+///     #if DEBUG
+///     static var mock: [Self] {
+///       [
+///             .init(name: DataGenerator.random().string(), surname: DataGenerator.random().string()),
+///             .init(name: DataGenerator.random().string(), surname: DataGenerator.random().string()),
+///             .init(name: DataGenerator.random().string(), surname: DataGenerator.random().string()),
+///       ]
+///     }
+///     #endif
+///}
+///```
+
 @attached(member, names: named(mock))
 public macro MockBuilder(
     numberOfItems: Int,
