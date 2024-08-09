59954b4340478137b7b1b4089835186847f8c95e
Rezo Joglidze <rezojoglidze7@gmail.com>
2024-08-09 21:34:59 +0400

    Add MockBuilderProperty macro description

--- Sources/RJSwiftMacros/Attached/MockBuilder/MockBuilder.swift
+++ Sources/RJSwiftMacros/Attached/MockBuilder/MockBuilder.swift
@@ -39,5 +39,38 @@ public macro MockBuilder(
     dataGeneratorType: DataGeneratorType
 ) = #externalMacro(module: "RJSwiftMacrosImpl", type: "MockBuilderMacro")
 
+
+/// - `MockBuilderProperty<T: Any>(value: T)`: This macro is used to set an initial value to a property within a struct or class. It is a generic macro, meaning it can accept any type (T) as a parameter. If the provided type is unsupported, a warning message will be displayed. This macro is particularly useful in scenarios where you need to initialize properties with specific values.
+/// ```
+///  import RJSwiftMacros
+///
+///  @MockBuilder(numberOfItems: 2, dataGeneratorType: .random)
+///  struct Person {
+///      @MockBuilderProperty(value: "John") let name: String
+///      let surname: String
+///
+///      #if DEBUG
+///      static var mock: Person {
+///          .init(
+///              name: MockBuilderSupportedType.generate(elementType: .string("John")) as! String,
+///              surname: MockBuilderSupportedType.generate(elementType: .string(), generatorType: .random) as! String
+///              )
+///      }
+///
+///      static var mockArray: [Person ] {
+///          [
+///              .init(
+///                  name: MockBuilderSupportedType.generate(elementType: .string("John")) as! String,
+///                  surname: MockBuilderSupportedType.generate(elementType: .string(), generatorType: .random) as! String
+///                  ),
+///              .init(
+///                  name: MockBuilderSupportedType.generate(elementType: .string("John")) as! String,
+///                  surname: MockBuilderSupportedType.generate(elementType: .string(), generatorType: .random) as! String
+///                  ),
+///          ]
+///      }
+///      #endif
+///  }
+///```
 @attached(peer, names: named(MockBuilderPropertyMacro))
 public macro MockBuilderProperty<T: Any>(value: T) = #externalMacro(module: "RJSwiftMacrosImpl", type: "MockBuilderPropertyMacro")
